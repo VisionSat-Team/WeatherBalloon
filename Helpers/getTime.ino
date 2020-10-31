a68433f1b52cf9dc025799e8ecbc5f02ec12b0bf
Emmanuel Nwogu <60305705+emmanuel-nwogu@users.noreply.github.com>
2020-10-30 22:24:44 -0500

    Create getTime.ino

---
+++ Helpers/getTime.ino
@@ -0,0 +1,32 @@
+#include <Adafruit_GPS.h>
+
+#include "SoftwareSerial.h"
+
+SoftwareSerial serial(3, 2);
+Adafruit_GPS GPS(&serial);
+
+/* time[0] = hours, time[1] = minutes, time[2] = seconds 
+Always update the time array using updateTime() before use */
+uint8_t time[3];
+
+#define GPSECHO false
+     
+boolean usingInterrupt = false;
+
+void setup() {
+  Serial.begin(115200);
+     
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+}
+
+void updateTime() {
+  GPS.parse(GPS.lastNMEA());
+  time[0] = GPS.hour;
+  time[1] = GPS.minute;
+  time[2] = GPS.seconds;
+}
+
+void loop() {}
