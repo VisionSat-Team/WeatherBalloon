7643fdf924b6634e84c005dcf892f9fb99a16870
tleblanc302 <62121950+tleblanc302@users.noreply.github.com>
2020-12-22 13:05:00 -0600

    Update Libraries.ino

--- VisionSat/Libraries.ino
+++ VisionSat/Libraries.ino
@@ -1,1 +1,39 @@
+/* MS5611:
+ctrl+shift+I to open library manager
+search MS5611
+instal
+
+
+Wire.h, SPI, SD, and math.h is preinstalled into arduino so you dont have to worry about searching for these.
+
+
+MS5xxx: 
+ctrl+shift+I to open library manager
+search MS5xxx
+instal
+
+
+Adafruit_MAX31865:
+open library manager
+search Adafruit_MAX31865
+
+
+ArduCAM and memorysaver:
+to be completely honest this short video explains it much better than i could
+https://www.youtube.com/watch?v=hybQpjwJ4aA&feature=youtu.be
+
+
+pt100rtd:
+Go to the github link at: https://github.com/drhaney/pt100rtd
+Download the zip file
+Move the downloaded zip file to the library sub folder of the arduino folder in your personal files.
+Open the arduino ide
+go to sketch
+include library
+add .zip file
+select pt100rtd
+
+
+
+
 
