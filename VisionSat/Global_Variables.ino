4abf0668c62105e2eecfd711b416c9a0d712c90c
Mandy Chiasson <51835778+mutant-llama1@users.noreply.github.com>
2021-01-14 15:34:53 -0600

    Update Global_Variables.ino

--- VisionSat/Global_Variables.ino
+++ VisionSat/Global_Variables.ino
@@ -1,1 +1,18 @@
 
+long timermillis;
+
+long timer;
+
+long millis;
+
+int zeroHex = 0x00;
+
+int chipsel = 10;
+
+String ThcMessage;
+
+long BeaconDelay = 60000;
+
+String SensorData= "";
+
+int centralStandardTime;
