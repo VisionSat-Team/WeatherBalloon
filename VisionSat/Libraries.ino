7fa84a7d830e7ac43bc8458a138fde2401814dcc
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2021-01-19 21:24:51 -0600

    Tuesday Meeting Update

--- VisionSat/Libraries.ino
+++ VisionSat/Libraries.ino
@@ -3,9 +3,7 @@ ctrl+shift+I to open library manager
 search MS5611
 instal
 
-
 Wire.h, SPI, SD, and math.h is preinstalled into arduino so you dont have to worry about searching for these.
-
 
 MS5xxx: 
 ctrl+shift+I to open library manager
@@ -33,7 +31,18 @@ include library
 add .zip file
 select pt100rtd
 
+*/
 
-
-
-
+#include <MS5611.h>
+#include <MS5xxx.h>
+#include <math.h>
+#include <Wire.h>
+#include <ArduCAM.h>
+#include <SPI.h>
+#include <pt100rtd.h>
+#include <Adafruit_MAX31865.h>
+#include <SD.h>
+#include <memorysaver.h>
+#include <MicroNMEA.h>  // Get current time from GPS 
+#include <SoftwareSerial.h> 
+#include <Adafruit_GPS.h>
