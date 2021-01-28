28d11b002c3af5612ec2c06b80d84023e065c873
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2021-01-28 16:51:47 -0600

    Update f_gps.ino

--- a_VisionSat21/f_gps.ino
+++ a_VisionSat21/f_gps.ino
@@ -39,3 +39,42 @@ String getLocation() {
   }
   return " Error No Fix";
 }
+void captureGPS() {
+  //Caputures the 2 NMEA sentences needed to parse the GPS data in the Adafruit GPS libary
+  int sentenceCount = 0;
+  while (sentenceCount <= 2) {
+    
+      GPS.read();
+
+    GPS.parse(GPS.lastNMEA());
+    sentenceCount += 1;
+  }
+
+}
+String getTime() {
+  //Turns the hours into standard time
+  int hours = GPS.hour - 6;
+  if (hours > 12) hours -= 12;
+  if (hours <= 0) hours += 12;
+
+  //String manlipulations for User to read easier
+  String minutes = String(GPS.minute);
+  String sec = String(GPS.seconds);
+  if (GPS.minute < 10) minutes = "0" + minutes;
+  if (GPS.seconds < 10) sec = "0" + sec;
+
+  return String(hours) + ":" + minutes + ":" + sec;
+
+}
+String getLocation() {
+  /*
+    The GPS fix informs if the GPS is recieving a signal from Satilites
+    and can be seen on our GPS Sensor as a bluelight.
+    If the blue light is not on there can be many reasons but I suggest first move locations.
+    Without the fix the GPS can not recieve any data.
+  */
+  if (GPS.fix) {
+    return String(GPS.latitudeDegrees ) + ", " + String(GPS.longitudeDegrees);
+  }
+  return " Error No Fix";
+}
