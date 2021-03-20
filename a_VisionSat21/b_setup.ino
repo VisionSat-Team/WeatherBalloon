64ea73d28d79149c23a891648d140111981e85c3
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2021-03-20 11:29:17 -0500

    3/20/2021 Update

--- a_VisionSat21/b_setup.ino
+++ a_VisionSat21/b_setup.ino
@@ -2,13 +2,20 @@
  *  Setup and Initialize all sensors */
 
 void setup() { 
+  
   // General START
-  Serial.begin(115200);
-  sensorData = "setup"; 
   fileName = "setup"; 
   beaconDelay = 5000; 
-  previousBeacon = 0; 
+  Serial.begin(115200);
+  Serial3.begin(115200);
+  Serial2.begin(115200);
+  Serial.println("setup started"); // Testing
+  previousMillis = 0; 
   // General END 
+
+  //TNC Start
+  port1.begin(19200);
+  //TNC END
 
 
   // Temperature START
@@ -28,48 +35,48 @@ void setup() {
   pressureSensor.ReadProm();
   pressureSensor.Readout();
   p_ref =  pressureSensor.GetPres();
-//  t_ref = lookUpTemperature();
+  t_ref = lookUpTemperature();
   //  Altimeter END
 
 
   // Camera START
-//  uint8_t temp;   //  temporary var
-//
-//  Serial.println(F("ACK CMD ArduCAM Start! END"));
-//
-//  pinMode(cameraCS, OUTPUT);    // set the CS as an output:
-//  digitalWrite(cameraCS, HIGH);
-//
-//  SPI.begin();    // initialize SPI:
-//
-//  while (1) { //  Check if the ArduCAM SPI bus is OK
-//  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
-//  temp = myCAM.read_reg(ARDUCHIP_TEST1);
-//
-//    if (temp != 0x55) {
-//      Serial.println(F("ACK CMD SPI interface Error! END"));
-//      delay(1000); 
-//      continue;
-//    } 
-//    else {
-//      Serial.println(F("ACK CMD SPI interface OK. END"));
-//      break;
-//    }
-//  }
-//
-//  //Change to JPEG capture mode and initialize the OV5642 module
-//  myCAM.set_format(JPEG);
-//  myCAM.InitCAM();
-//
-//  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);    // VSYNC is active HIGH
-//  myCAM.OV5642_set_JPEG_size(OV5642_2592x1944);       //  resolution
-//  // Camera END
+  delay(5000); 
+  
+  uint8_t temp;   //  temporary var
+
+  Serial.println(F("ACK CMD ArduCAM Start! END"));
+
+  pinMode(cameraCS, OUTPUT);    // set the CS as an output:
+  digitalWrite(cameraCS, HIGH);
+
+  SPI.begin();    // initialize SPI:
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  while(1) {
+    myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+    temp = myCAM.read_reg(ARDUCHIP_TEST1);
+    serial.
+    if (temp != 0x55){
+      Serial.println(F("ACK CMD SPI interface Error! END"));
+      delay(1000); continue;
+    } else {
+      Serial.println(F("ACK CMD SPI interface OK. END"));
+      break;
+    }
+  }
+
+  //Change to JPEG capture mode and initialize the OV5642 module
+  myCAM.set_format(JPEG);
+  myCAM.InitCAM();
+
+  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);    // VSYNC is active HIGH
+  myCAM.OV5642_set_JPEG_size(OV5642_2592x1944);       //  resolution
+  // Camera END
 
 
   //  SD Card START  
-  SD.begin(); 
+  SD.begin(SD_CS); 
   fileName = "setup";
-  lastfileName = "setup";
+  lastFileName = "setup";
   duplicate = 0;
   // SD Card END   
 
@@ -79,9 +86,12 @@ void setup() {
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PGCMD_ANTENNA);
+  GPS.sendCommand(PMTK_FR_MODE);
   delay(1000);
   // GPS END
 
    
   Serial.println("setup finished"); // Testing
+
+  delay(beaconDelay);
 }
