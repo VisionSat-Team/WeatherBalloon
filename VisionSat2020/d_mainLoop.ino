1889664ce37241b37e21bb2f17d63d18f49a65a1
AlexPT36 <62034488+AlexPT36@users.noreply.github.com>
2021-04-15 18:33:07 -0500

    4-15-21

--- VisionSat2020/d_mainLoop.ino
+++ VisionSat2020/d_mainLoop.ino
@@ -11,56 +11,48 @@ void loop() {
 
   // True if it has been (beaconDelay) time since last beacon
   if (millis() - beaconDelay >= previousMillis) {
-    // Capture Sensor Data 
-//    String sensorData = captureData();
-    String sensorData = " "; 
-    
+    // Capture Sensor Data
+    String sensorData = captureData();
+    //    String sensorData = "";
 
-    // Beacon TNC data to ground station 
+    // Beacon TNC data to ground station
     keyUp(sensorData);
 
     // Start new file name "####"
     String genFileName = newFileName(); // General file name
 
-    // Create Names for each file 
+    // Create Names for each file
     String dataFileName = genFileName + ".txt";      // "####.txt"
-    String picFileName =  genFileName + "pic.txt";   // "####pic.txt" 
-
-    Serial.println(sensorData); 
-    Serial.println(dataFileName); 
-    Serial.println(picFileName);
-    Serial.println("  end of main loop"); 
-    Serial.println(); 
+    String picFileName =  genFileName + "pic.txt";   // "####pic.txt"
 
     // Save sensorData to SD card
     saveData(sensorData, dataFileName);
 
-    // Take Picture and save to SD Card 
-    takePicture(picFileName); 
+    // Take Picture and save to SD Card
+    takePicture(picFileName);
 
-    // Update Timer 
+    // Update Timer
     previousMillis = millis();
 
-    // Testing 
-    
+    // Testing
+    Serial.println(sensorData);
+    Serial.println(dataFileName);
+    Serial.println(picFileName);
+    Serial.println("  end of main loop");
+    Serial.println();
   }
 
-  // Check if TNC has a message 
+  // Check if TNC has a message
   captureGroundStation();
 }
-
 
 
 String captureData() {
   // Capture Data from each Sensor
   String temperatureData = String(toFarenheit());
-   captureCommandStation()
   String gpsData = String(getLocation());
-  captureCommandStation()
-   String altimeterData = String(getPressure());
-   captureCommandStation()
+  String altimeterData = String(getPressure());
   String timeData = (getTime());
-    captureCommandStation()
   // SDAvailable? ****
   // Wire Cut? ****
 
