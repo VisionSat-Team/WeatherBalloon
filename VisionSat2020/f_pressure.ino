c38c3fd1e858046cc2cce3bade3dadeb519b0544
AlexPT36 <62034488+AlexPT36@users.noreply.github.com>
2021-04-17 14:34:57 -0500

    4-17-2021

---
+++ VisionSat2020/f_pressure.ino
@@ -0,0 +1,16 @@
+/*
+    pressure
+
+    Pressure Functions:
+      - getPressure(): returns pressure as a string 
+*/
+
+String getPressure()
+{
+  // Update Altimeter Data
+  pressureSensor.ReadProm();
+  pressureSensor.Readout();
+
+  // Read & Return Pressure
+  return String(pressureSensor.GetPres()); 
+}
