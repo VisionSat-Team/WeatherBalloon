bf669ba61a8f0b89cd9503bf58fb522a421692bc
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2021-09-07 20:21:11 -0500

    9/7/21
    -Added two additional beacon delays that begins at thirty seconds, forty, and then sixty seconds.
    -Added communication and a slowed down SPI rate in the General portion
    -Added initial try function
    -Eliminated the numSamples and meanPreassure from the altimeter portion
    -Eliminated the following from the camera portion:
    
      delay(5000) 
      uint8_t temp
      Serial.println(F("ACK CMD ArduCAM Start! END"))
      SPI.begin() 
      myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
      temp = myCAM.read_reg(ARDUCHIP_TEST1);
        
        if (temp != 0x55){
          Serial.println(F("ACK CMD SPI interface Error! END"));
          delay(1000); continue;
        } else {
          Serial.println(F("ACK CMD SPI interface OK. END"));
          break;
    - SD Card variable changes: 
    fileName, lastFileName, and duplicate were removed
    -Added APRS functions
    -Added Cut Down functions
    -Added Airborne Mode function for GPS 
    -Remade the testing function which originally read as: Serial.println("setup finished");
    
    -Remade

--- a_VisionSat21/b_setup.ino
+++ a_VisionSat21/b_setup.ino
@@ -1,96 +1,113 @@
-/* Setup Tab
- *  Setup and Initialize all sensors */
+/*
+    setup
 
-void setup() { 
-  
+    Set up sensors, initialize global variables, etc.
+*/
+
+void setup() {
+
   // General START
-  fileName = "setup"; 
-  beaconDelay = 5000; 
+   beaconDelay[0] = 30;
+  beaconDelay[1] = 45;
+  beaconDelay[2] = 60; // 60 second delay between beacons
   Serial.begin(115200);
   Serial3.begin(115200);
   Serial2.begin(115200);
-  Serial.println("setup started"); // Testing
-  previousMillis = 0; 
-  // General END 
+  previousMillis = 0;
+  Serial.println(); 
+  // Start Communication
+  Wire.begin();
+  SPI.begin();
+  // Slow down SPI transfer rate (for Camera)
+  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
+  // Temporary variable for repeatedly attempting to initialize sentences
+  int initTries = 5;
+  // General END
 
-  //TNC Start
-  port1.begin(19200);
-  //TNC END
+
+  // GPS START
+  // Try initializing sensor
+  initTries = 5;
+  while (!GPS.begin(9600) && initTries > 0) {
+    initTries--;
+  }
+  //Send a SetNav string to command the GPS to set it in "Airborne Mode" to be used above 100,000 feet.
+  //-------------------------------------------------------------------------------------------------------------------------------------------------
+  unsigned char setNav[] = {
+    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00,
+    0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC
+  };
+
+  //  Set uBlox MAX series to Airborne Mode.
+  for (int i = 0; i < 44; i++) {
+    Serial3.write(setNav[i]);
+  }
+  delay(1000);
+  // GPS END
 
 
-  // Temperature START
-  thermo.begin(MAX31865_3WIRE);
-  // Temperature END 
-
-  
   //  Altimeter START
-  int numSamples = 20;  
-  int meanPressure = 0;
   pressureSensor.setI2Caddr(MS5607_ADDRESS);
-  if (pressureSensor.connect() > 0) {
-    Serial.println("Error connecting...");
-    delay(200);
-//    setup();
+  // Try initializing sensor
+  initTries = 5;
+  while (!pressureSensor.connect() && initTries > 0) {
+    initTries--;
   }
   pressureSensor.ReadProm();
   pressureSensor.Readout();
   p_ref =  pressureSensor.GetPres();
-  t_ref = lookUpTemperature();
   //  Altimeter END
+
+  // Temperature START
+  thermo.begin(MAX31865_3WIRE);
+  // Temperature END
 
 
   // Camera START
-  delay(5000); 
-  
-  uint8_t temp;   //  temporary var
-
-  Serial.println(F("ACK CMD ArduCAM Start! END"));
-
-  pinMode(cameraCS, OUTPUT);    // set the CS as an output:
+  pinMode(cameraCS, OUTPUT);
+  // set the CS as an output:
   digitalWrite(cameraCS, HIGH);
 
-  SPI.begin();    // initialize SPI:
-  SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
-  while(1) {
-    myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
-    temp = myCAM.read_reg(ARDUCHIP_TEST1);
-    
-    if (temp != 0x55){
-      Serial.println(F("ACK CMD SPI interface Error! END"));
-      delay(1000); continue;
-    } else {
-      Serial.println(F("ACK CMD SPI interface OK. END"));
-      break;
-    }
+  // Try initializing sensor
+  initTries = 5;
+  while (!setCam() && initTries > 0) {
+    initTries--;
   }
-
-  //Change to JPEG capture mode and initialize the OV5642 module
-  myCAM.set_format(JPEG);
-  myCAM.InitCAM();
-
-  myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);    // VSYNC is active HIGH
-  myCAM.OV5642_set_JPEG_size(OV5642_2592x1944);       //  resolution
   // Camera END
 
 
-  //  SD Card START  
-  SD.begin(SD_CS); 
-  fileName = "setup";
-  lastFileName = "setup";
-  duplicate = 0;
-  // SD Card END   
+  //  SD Card START
+  initTries = 5;
+  while (!SDAvailable && initTries > 0) {
+    SDAvailable = SD.begin(SD_CS);
+    initTries--;
+  }
+  // SD Card END
 
+
+  //TNC Start
+  port1.begin(19200);
+  //TNC END
   
-  // GPS START 
-  GPS.begin(9600);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PGCMD_ANTENNA);
-  delay(1000);
-  // GPS END
+  //APRS Start
+  int aprsReadPin = 38; 
+  int powerRelayPin = 39;
+  pinMode(aprsReadPin, INPUT);
+  pinMode(powerRelayPin, OUTPUT);
+  //APRS END
 
-   
-  Serial.println("setup finished"); // Testing
+  // Cut Down START
+  pinMode(burnWirePin, OUTPUT);
+  digitalWrite(burnWirePin, LOW);
+  // Cut Down END
 
-  delay(beaconDelay);
+
+  //Testing
+  ResetEEPROM();
+  beaconDelay[0] = 30;
+  beaconDelay[1] = 45;
+  beaconDelay[2] = 60;
+  whichDelay = 0;
+  Serial.println("*** End of setup ***");
+  Serial.println(); 
 }
