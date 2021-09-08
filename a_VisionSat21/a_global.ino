f20e7ffe04b0cd7ddb76261488f1027e422e02e9
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2021-09-07 19:54:53 -0500

    added three seconds to beacon delay

--- a_VisionSat21/a_global.ino
+++ a_VisionSat21/a_global.ino
@@ -1,20 +1,22 @@
-/* global
- *  Global variable declaration */
+/* 
+ *  globalVariables 
+ *  
+ *  Global Variable Declaration 
+ */
 
 // General START
-long beaconDelay;
+long beaconDelay[3]; //number of seconds to delay for beacon
+int whichDelay; //selector for which beacon delay value to use
 long previousMillis; // Last Beacon Time
-String sensorData;
 // General END
 
 
-// TNC START
-int zeroHex = 0x00;
-byte header[25] = {0xC0, zeroHex, 0xAE, 0x6A, 0xAA, 0x98, 0x40, 0x40, 0xF6, 0xAE, 0x6A, 0xAA,
-                   0x98, 0x40, 0x40, 0x60, 0x86, 0xA2, 0x40, 0x40, 0x40, 0x40, 0x61, 0x03, 0xF0};
-String tncMessage;
-SoftwareSerial port1(10,11); // RX, TX
-// TNC END 
+// GPS START 
+//SoftwareSerial serial(12, 11); // RX, TX
+Adafruit_GPS GPS(&Serial3);
+int centralStandardTime;
+#define PMTK_FR_MODE "$PMTK886,3*2B"
+// GPS END 
 
 
 // Altimeter START 
@@ -22,7 +24,7 @@ SoftwareSerial port1(10,11); // RX, TX
 #define MS5607_ADDRESS 0x76
 MS5xxx pressureSensor(&Wire);
 // Set the reference pressure to the current pressure to get relative altitude changes
-double pressure, temperature, p_ref, t_ref, altitude, meanPressure, numSamples;
+double p_ref, meanPressure, numSamples;
 char receivedChar;
 bool newData = false;
 // Altimeter END 
@@ -30,7 +32,6 @@ bool newData = false;
 
 // Temperature START 
 // Using Hardware SPI (Sensor --> Mega Pin #) cs --> 10, SDI --> MOSI (51), SDO --> MISO (50), CLK --> SCK (52)
-//Software SPI is bad its too slow and relies on bitbanging.
 Adafruit_MAX31865 thermo = Adafruit_MAX31865(9);
 // The value of the Rref resistor. We are using the PT100 whose value is 430
 #define RREF      430.0
@@ -41,24 +42,33 @@ pt100rtd PT100 = pt100rtd();
 // Temperature END 
 
 
-// GPS START 
-//SoftwareSerial serial(12, 11); // RX, TX
-Adafruit_GPS GPS(&Serial3);
-int centralStandardTime;
-// GPS END 
-
-
 // Camera Variables START
 const int cameraCS = 7; 
 ArduCAM myCAM(OV5642, cameraCS);
 bool is_header = false;
-uint8_t read_fifo_burst(ArduCAM myCAM);
+uint8_t read_fifo_burst(ArduCAM myCAM, String picFileName);
 // Camera Variable END 
 
 
 // SD Card START 
 const int SD_CS = 53; 
-String fileName;
-String lastFileName;
-int duplicate;
+const int dataSaveCountAddress = 0; 
+const int dataSaveRolloverAddress = 1; 
+boolean SDAvailable = false; 
 // SD Card END 
+
+
+// TNC START
+bool listenNow = false;
+int zeroHex = 0x00;
+byte header[25] = {0xC0, zeroHex, 0xAE, 0x6A, 0xAA, 0x98, 0x40, 0x40, 0xF6, 0xAE, 0x6A, 0xAA,
+                   0x98, 0x40, 0x40, 0x60, 0x86, 0xA2, 0x40, 0x40, 0x40, 0x40, 0x61, 0x03, 0xF0};
+String tncMessage;
+SoftwareSerial port1(10,11); // RX, TX
+// TNC END 
+
+
+// Cut Down START
+const int burnWirePin = 27; 
+boolean isCutDown = false; 
+// Cut Down END
