2b8ac4ca8d341bff3690827226e73a63029009e0
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2021-09-07 19:44:38 -0500

    Declared the fumctions

--- a_VisionSat21/a_VisionSat21.ino
+++ a_VisionSat21/a_VisionSat21.ino
@@ -11,3 +11,21 @@
 #include <MicroNMEA.h>  // Get current time from GPS 
 #include <SoftwareSerial.h> 
 #include <Adafruit_GPS.h>
+
+boolean setCam();
+ void ResetEEPROM();
+ String captureData();
+ void keyUp(String messageToGround);
+ String newFileName();
+ void saveData (String sensorData, String fileName);
+ void takePicture(String picFileName);
+ void captureGroundStation();
+ float toFarenheit();
+ float lookUpTemperature();
+ String getLocation();
+ void captureGPS();
+ String getTime();
+ String getPressure(); 
+ String getFeetPerSec();
+ void burnWire();
+
